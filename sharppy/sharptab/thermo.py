8d853056150b1b53519d337ba0fef7e808ab9926
Patrick Marsh <patrickmarshwx@gmail.com>
2013-01-15 20:12:35 -0600

    Added wobus function

--- sharppy/sharptab/thermo.py
+++ sharppy/sharptab/thermo.py
@@ -28,6 +28,45 @@ def theta(p, t, p2=1000.):
     return ((t + ZEROCNK) * (p2 / p)**ROCP) - ZEROCNK
 
 
+def wobf(t):
+    '''
+    Implementation of the Wobus Function for computing the moist adiabats.
+
+    Parameters
+    ----------
+    t : float, numpy array
+        Temperature (C)
+
+    Returns
+    -------
+    Correction to theta (C) for calculation of saturated potential temperature.
+
+    '''
+    t -= 20
+
+    npol = 1 + t * (-8.841660499999999e-3 + t * ( 1.4714143e-4
+           + t * (-9.671989000000001e-7 + t * (-3.2607217e-8
+           + t * (-3.8598073e-10)))))
+    npol = 15.13 / (npol**4)
+
+    ppol = t * (4.9618922e-07 + t * (-6.1059365e-09 +
+          t * (3.9401551e-11 + t * (-1.2588129e-13 +
+          t * (1.6688280e-16)))))
+    ppol = 1 + t * (3.6182989e-03 + t * (-1.3603273e-05 + ppol))
+    ppol = (29.93 / (ppol**4)) + (0.96 * t) - 14.8
+
+    try:
+        correction = np.zeros(t.shape, dtype=np.float64)
+        correction[t <= 0] = npol[t <= 0]
+        correction[t > 0] = ppol[t > 0]
+        return correction
+    except AttributeError:
+        if t <= 0:
+            return npol
+        else:
+            return ppol
+
+
 def ctof(t):
     '''
     Convert temperature from Celsius to Fahrenheit
