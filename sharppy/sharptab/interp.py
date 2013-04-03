65eb45e526ba7b15d0bda936358ca56b8893cbaa
Patrick Marsh <patrickmarshwx@gmail.com>
2013-04-03 10:03:44 -0500

    Major Rewrite of Interpolation Routines
    Changed order of parameters passed to interpolation routines; added generic interpolation routines; added ability to handle masked data

--- sharppy/sharptab/interp.py
+++ sharppy/sharptab/interp.py
@@ -11,35 +11,35 @@ __all__ = ['pres', 'hght', 'temp', 'dwpt', 'vtmp', 'components', 'vec']
 __all__ += ['agl', 'msl']
 
 
-def pres(h, prof):
+def pres(prof, h):
     '''
     Interpolates the given data to calculate a pressure at a given height
 
     Parameters
     ----------
-    h : number, numpy array
-        Height (m) of the level for which pressure is desired
     prof : profile object
         Profile object
+    h : number, numpy array
+        Height (m) of the level for which pressure is desired
 
     Returns
     -------
     Pressure (hPa) at the given height
 
     '''
-    return 10**np.interp(h, prof.hght, prof.logp)
+    return generic_interp_hght(h, prof.hght, prof.logp, log=True)
 
 
-def hght(p, prof):
+def hght(prof, p):
     '''
     Interpolates the given data to calculate a height at a given pressure
 
     Parameters
     ----------
-    p : number, numpy array
-        Pressure (hPa) of the level for which height is desired
     prof : profile object
         Profile object
+    p : number, numpy array
+        Pressure (hPa) of the level for which height is desired
 
     Returns
     -------
@@ -50,19 +50,19 @@ def hght(p, prof):
     # routine to be in ascending order. Because pressure decreases in the
     # vertical, we must reverse the order of the two arrays to satisfy
     # this requirement.
-    return np.interp(np.log10(p), prof.logp[::-1], prof.hght[::-1])
+    return generic_interp_pres(np.log10(p), prof.logp[::-1], prof.hght[::-1])
 
 
-def temp(p, prof):
+def temp(prof, p):
     '''
     Interpolates the given data to calculate a temperature at a given pressure
 
     Parameters
     ----------
-    p : number, numpy array
-        Pressure (hPa) of the level for which temperature is desired
     prof : profile object
         Profile object
+    p : number, numpy array
+        Pressure (hPa) of the level for which temperature is desired
 
     Returns
     -------
@@ -73,20 +73,20 @@ def temp(p, prof):
     # routine to be in ascending order. Because pressure decreases in the
     # vertical, we must reverse the order of the two arrays to satisfy
     # this requirement.
-    return np.interp(np.log10(p), prof.logp[::-1], prof.tmpc[::-1])
+    return generic_interp_pres(np.log10(p), prof.logp[::-1], prof.tmpc[::-1])
 
 
-def dwpt(p, prof):
+def dwpt(prof, p):
     '''
     Interpolates the given data to calculate a dew point temperature
     at a given pressure
 
     Parameters
     ----------
-    p : number, numpy array
-        Pressure (hPa) of the level for which dew point temperature is desired
     prof : profile object
         Profile object
+    p : number, numpy array
+        Pressure (hPa) of the level for which dew point temperature is desired
 
     Returns
     -------
@@ -97,48 +97,46 @@ def dwpt(p, prof):
     # routine to be in ascending order. Because pressure decreases in the
     # vertical, we must reverse the order of the two arrays to satisfy
     # this requirement.
-    return np.interp(np.log10(p), prof.logp[::-1], prof.dwpc[::-1])
+    return generic_interp_pres(np.log10(p), prof.logp[::-1], prof.dwpc[::-1])
 
 
-def vtmp(p, prof):
+def vtmp(prof, p):
     '''
     Interpolates the given data to calculate a virtual temperature
     at a given pressure
 
     Parameters
     ----------
-    p : number, numpy array
-        Pressure (hPa) of the level for which virtual temperature is desired
     prof : profile object
         Profile object
+    p : number, numpy array
+        Pressure (hPa) of the level for which virtual temperature is desired
 
     Returns
     -------
     Virtual tmperature (C) at the given pressure
 
     '''
-    t = temp(p, prof)
-    td = dwpt(p, prof)
+    t = temp(prof, p)
+    td = dwpt(prof, p)
     try:
-        vt = []
-        for pp,tt,tdtd in zip(p, t, td):
-            vt.append(thermo.virtemp(pp, tt, tdtd))
-        return np.asanyarray(vt)
+        vt = [thermo.virtemp(pp, tt, tdtd) for pp,tt,tdtd in zip(p, t, td)]
+        return ma.asarray(vt)
     except TypeError:
         return thermo.virtemp(p, t, td)
 
 
-def components(p, prof):
+def components(prof, p):
     '''
     Interpolates the given data to calculate the U and V components at a
     given pressure
 
     Parameters
     ----------
-    p : number, numpy array
-        Pressure (hPa) of a level
     prof : profile object
         Profile object
+    p : number, numpy array
+        Pressure (hPa) of a level
 
     Returns
     -------
@@ -148,8 +146,8 @@ def components(p, prof):
     # routine to be in ascending order. Because pressure decreases in the
     # vertical, we must reverse the order of the two arrays to satisfy
     # this requirement.
-    U = np.interp(np.log10(p), prof.logp[::-1], prof.u[::-1])
-    V = np.interp(np.log10(p), prof.logp[::-1], prof.v[::-1])
+    U = generic_interp_pres(np.log10(p), prof.logp[::-1], prof.u[::-1])
+    V = generic_interp_pres(np.log10(p), prof.logp[::-1], prof.v[::-1])
     return U, V
 
 
@@ -173,7 +171,7 @@ def vec(p, prof):
     return utils.comp2vec(U, V)
 
 
-def agl(h, prof):
+def agl(prof, h):
     '''
     Convert a height from mean sea-level (MSL) to above ground-level (AGL)
 
@@ -192,7 +190,7 @@ def agl(h, prof):
     return h - prof.hght[prof.sfc]
 
 
-def msl(h, prof):
+def msl(prof, h):
     '''
     Convert a height from above ground-level (AGL) to mean sea-level (MSL)
 
@@ -209,6 +207,76 @@ def msl(h, prof):
 
     '''
     return h + prof.hght[prof.sfc]
+
+
+def generic_interp_hght(h, hght, field, log=False):
+    '''
+    Generic interpolation routine
+
+    Parameters
+    ----------
+    h : number, numpy array
+        Height (m) of the level for which pressure is desired
+    hght : numpy array
+        The array of heights
+    field : numpy array
+        The variable which is being interpolated
+    log : bool
+        Flag to determine whether the 'field' variable is in log10 space
+
+    Returns
+    -------
+    Value of the 'field' variable at the given height
+
+    '''
+    if ma.isMaskedArray(hght):
+        not_masked1 = ~hght.mask
+    else:
+        not_masked1 = np.ones(hght.shape)
+    if ma.isMaskedArray(field):
+        not_masked2 = ~field.mask
+    else:
+        not_masked2 = np.ones(field.shape)
+    not_masked = not_masked1 * not_masked2
+    if log:
+        return 10**np.interp(h, hght[not_masked], field[not_masked],
+                             left=ma.masked, right=ma.masked)
+    else:
+        return np.interp(h, hght[not_masked], field[not_masked],
+                         left=ma.masked, right=ma.masked)
+
+
+def generic_interp_pres(p, pres, field):
+    '''
+    Generic interpolation routine
+
+    Parameters
+    ----------
+    p : number, numpy array
+        Pressure (hPa) of the level for which the field variable is desired
+    pres : numpy array
+        The array of pressure
+    field : numpy array
+        The variable which is being interpolated
+    log : bool
+        Flag to determine whether the 'field' variable is in log10 space
+
+    Returns
+    -------
+    Value of the 'field' variable at the given pressure
+
+    '''
+    if ma.isMaskedArray(pres):
+        not_masked1 = ~pres.mask
+    else:
+        not_masked1 = np.ones(pres.shape)
+    if ma.isMaskedArray(field):
+        not_masked2 = ~field.mask
+    else:
+        not_masked2 = np.ones(field.shape)
+    not_masked = not_masked1 * not_masked2
+    return np.interp(p, pres[not_masked], field[not_masked], left=ma.masked,
+                     right=ma.masked)
 
 
 
--- sharppy/tests/test_interp.py
+++ sharppy/tests/test_interp.py
@@ -4,136 +4,131 @@ import numpy.testing as npt
 import sharppy.sharptab.interp as interp
 from sharppy.sharptab.utils import vec2comp
 from sharppy.sharptab.profile import Profile
+import test_profile as tp
 
 
-p = [1000., 925., 850., 700., 500., 300, 250., 200., 150., 100.]
-z = [161., 798., 1496., 3086., 5720., 9370., 10570., 11970., 13700., 16230.]
-t = [-9999., 1.2, 9.2, 1.6, -15.5, -43.7, -54.5, -64.1, -64.7, -58.7]
-d = [-9999., 1., 9.2, -1.2, -21.5, -50.7, -62.5, -71.1, -71.7, -73.7]
-wd = [-9999., 70., 175., 230., 255., 265., 255., 260., 280., 295.]
-ws = [-9999., 31.99, 27.99, 25.99, 25.99, 48., 52.99, 75., 77., 39.01]
-prof = Profile(pres=p, hght=z, tmpc=t, dwpc=d, wdir=wd, wspd=ws)
+prof = tp.TestProfile().prof
 
 
 def test_pres():
     input_z = 1000.
-    correct_p = 902.639252576
-    returned_p = interp.pres(input_z, prof)
+    correct_p = 903.8343884049208
+    returned_p = interp.pres(prof, input_z)
     npt.assert_almost_equal(returned_p, correct_p)
 
     input_z = [1000., 3000., 6000.]
-    correct_p = np.asarray([902.639252576, 707.38979834, 480.785620453])
-    returned_p = interp.pres(input_z, prof)
+    correct_p = np.asarray([903.834388405, 710.02200544, 482.16636819])
+    returned_p = interp.pres(prof, input_z)
     npt.assert_almost_equal(returned_p, correct_p)
 
 
 def test_hght():
     input_p = 900
-    correct_z = 1024.17165016
-    returned_z = interp.hght(input_p, prof)
+    correct_z = 1036.0022240687013
+    returned_z = interp.hght(prof, input_p)
     npt.assert_almost_equal(returned_z, correct_z)
 
     input_p = [900, 800, 600, 400]
-    correct_z = np.asarray([1024.17165016, 1992.47263808,
-                            4292.73519676, 7314.42659961])
-    returned_z = interp.hght(input_p, prof)
+    correct_z = np.asarray([1036.0022240687013, 2020.3374024,
+                            4330.6739399, 7360.])
+    returned_z = interp.hght(prof, input_p)
     npt.assert_almost_equal(returned_z, correct_z)
 
 
 def test_temp():
     input_p = 900
-    correct_t = 3.7922252167
-    returned_t = interp.temp(input_p, prof)
+    correct_t = 14.589978853117268
+    returned_t = interp.temp(prof, input_p)
     npt.assert_almost_equal(returned_t, correct_t)
 
     input_p = [900, 800, 600, 400]
-    correct_t = np.asarray([3.7922252167, 6.82692323938,
-                            -6.23415788333, -27.8185835915])
-    returned_t = interp.temp(input_p, prof)
+    correct_t = np.asarray([14.589978853117268, 8.3624187,
+                            -7.48619696, -29.7])
+    returned_t = interp.temp(prof, input_p)
     npt.assert_almost_equal(returned_t, correct_t)
 
 
 def test_dwpt():
     input_p = 900
-    correct_t = 3.65703084712
-    returned_t = interp.dwpt(input_p, prof)
+    correct_t = 11.52599445805832
+    returned_t = interp.dwpt(prof, input_p)
     npt.assert_almost_equal(returned_t, correct_t)
 
     input_p = [900, 800, 600, 400]
-    correct_t = np.asarray([3.65703084712, 5.95263180125,
-                            -10.5001991246, -34.2554127969])
-    returned_t = interp.dwpt(input_p, prof)
+    correct_t = np.asarray([11.52599445805832, 6.07174951,
+                            -13.34348479, -35.7])
+    returned_t = interp.dwpt(prof, input_p)
     npt.assert_almost_equal(returned_t, correct_t)
 
 
 def test_vtmp():
     input_p = 900
-    correct_v = 4.722189142078207
-    returned_v = interp.vtmp(input_p, prof)
+    correct_v = 16.248060772724727
+    returned_v = interp.vtmp(prof, input_p)
     npt.assert_almost_equal(returned_v, correct_v)
 
     input_p = [900, 800, 600, 400]
-    correct_v = np.asarray([4.722189142078207, 8.069359717080374,
-                            -5.769345740514382, -27.74007032980404])
-    returned_v = interp.vtmp(input_p, prof)
+    correct_v = np.asarray([16.248060772724727, 9.62205272,
+                            -7.11816357, -29.63245875])
+    returned_v = interp.vtmp(prof, input_p)
     npt.assert_almost_equal(returned_v, correct_v)
 
 
 def test_components():
     input_p = 900
-    correct_u, correct_v = -21.1106953677, 1.63907594278
+    correct_u, correct_v = -5.53976475, 20.6746835
     correct = [correct_u, correct_v]
-    returned = interp.components(input_p, prof)
+    returned = interp.components(prof, input_p)
     npt.assert_almost_equal(returned, correct)
 
     input_p = [900, 800, 600, 400]
-    correct_u = np.asarray([-21.1106953677, 4.53891277421,
-                            22.289483447, 35.026084823])
-    correct_v = np.asarray([1.63907594278, 24.3933682556,
-                            12.1341348539, 5.61574926111])
+    correct_u = np.asarray([-5.53976475, 5.95267234,
+                            23.10783339, 42.])
+    correct_v = np.asarray([20.6746835, 15.54170573,
+                            -9.37502817, 0])
     correct = [correct_u, correct_v]
-    returned = interp.components(input_p, prof)
+    returned = interp.components(prof, input_p)
     npt.assert_almost_equal(returned, correct)
 
 
 def test_vec():
     input_p = 900
-    correct_wdir, correct_wspd = 94.43965025418073, 21.174230301336348
+    correct_wdir, correct_wspd = 165., 21.40400736
     correct = [correct_wdir, correct_wspd]
-    returned = interp.vec(input_p, prof)
+    returned = interp.vec(prof, input_p)
     npt.assert_almost_equal(returned, correct)
 
     input_p = [900, 800, 600, 400]
-    correct_wdir = np.asarray([94.43965025418073, 190.54057243918774,
-                               241.43664290047258, 260.8912611327822])
-    correct_wspd = np.asarray([21.174230301336348, 24.812056424754488,
-                               25.378303745260787, 35.473416212590074])
+    correct_wdir = np.asarray([165., 200.95747979,
+                               292.08277812, 270.])
+    correct_wspd = np.asarray([21.40400736, 16.64268383,
+                               24.93718343, 42.])
     correct = [correct_wdir, correct_wspd]
-    returned = interp.vec(input_p, prof)
+    returned = interp.vec(prof, input_p)
     npt.assert_almost_equal(returned, correct)
 
 
 def test_agl():
     input_z = 1000.
-    correct_agl = 202.
-    returned_agl = interp.agl(input_z, prof)
+    correct_agl = 643.0
+    returned_agl = interp.agl(prof, input_z)
     npt.assert_almost_equal(returned_agl, correct_agl)
 
     input_z = [1000., 3000., 6000.]
-    correct_agl = [202., 2202., 5202]
-    returned_agl = interp.agl(input_z, prof)
+    correct_agl = [643., 2643., 5643.]
+    returned_agl = interp.agl(prof, input_z)
     npt.assert_almost_equal(returned_agl, correct_agl)
 
 
 def test_msl():
     input_z = 1000.
-    correct_agl = 1798.
-    returned_agl = interp.msl(input_z, prof)
+    correct_agl = 1357.
+    returned_agl = interp.msl(prof, input_z)
     npt.assert_almost_equal(returned_agl, correct_agl)
 
     input_z = [1000., 3000., 6000.]
-    correct_agl = [1798., 3798., 6798]
-    returned_agl = interp.msl(input_z, prof)
+    correct_agl = [1357., 3357., 6357]
+    returned_agl = interp.msl(prof, input_z)
     npt.assert_almost_equal(returned_agl, correct_agl)
 
 
