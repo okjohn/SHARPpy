f19874709ee9d7e26c28e73764c8594885810cf2
Patrick Marsh <patrickmarshwx@gmail.com>
2013-04-02 20:38:48 -0500

    Added new hght interpolation function

--- sharppy/sharptab/interp.py
+++ sharppy/sharptab/interp.py
@@ -6,13 +6,16 @@ import sharppy.sharptab.thermo as thermo
 from sharppy.sharptab.constants import *
 
 
+__all__ = ['pres', 'hght']
+
+
 def pres(h, prof):
     '''
     Interpolates the given data to calculate a pressure at a given height
 
     Parameters
     ----------
-    h : number
+    h : number, numpy array
         Height (m) of the level for which pressure is desired
     prof : profile object
         Profile object
@@ -24,3 +27,25 @@ def pres(h, prof):
     '''
     return 10**np.interp(h, prof.hght, prof.logp)
 
+
+def hght(p, prof):
+    '''
+    Interpolates the given data to calculate a height at a given pressure
+
+    Parameters
+    ----------
+    p : number, numpy
+        Pressure (hPa) of the level for which height is desired
+    prof : profile object
+        Profile object
+
+    Returns
+    -------
+    Height (m) at the given pressure
+
+    '''
+    # Note: numpy's interpoloation routine expects the interpoloation
+    # routine to be in ascending order. Because pressure decreases in the
+    # vertical, we must reverse the order of the two arrays to satisfy
+    # this requirement.
+    return np.interp(np.log10(p), prof.logp[::-1], prof.hght[::-1])
