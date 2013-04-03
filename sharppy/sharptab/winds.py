4c0baec725386de5a1c44a1cc65f544b382e46b7
Patrick Marsh <patrickmarshwx@gmail.com>
2013-04-02 22:16:51 -0500

    Added test and function to compute the mean pressure weighted wind of a given layer

---
+++ sharppy/sharptab/winds.py
@@ -0,0 +1,65 @@
+''' Wind Manipulation Routines '''
+from __future__ import division
+import numpy as np
+import numpy.ma as ma
+from sharppy.sharptab import interp, utils
+from sharppy.sharptab.constants import *
+
+
+__all__ = ['mean_wind']
+
+
+def mean_wind(prof, pbot=850., ptop=250., psteps=20, stu=0, stv=0):
+    '''
+    Calculates a pressure-weighted mean wind through a layer. The default
+    layer is 850 to 200 hPa.
+
+    Parameters
+    ----------
+    prof: profile object
+        Profile Object
+    pbot : number (optional; default 850 hPa)
+        Pressure of the bottom level (hPa)
+    ptop : number (optional; default 250 hPa)
+        Pressure of the top level (hPa)
+    psteps : integer (optional; default 20)
+        Number of steps to loop through (int)
+    stu : number (optional; default 0)
+        U-component of storm-motion vector
+    stv : number (optional; default 0)
+        V-component of storm-motion vector
+
+    Returns
+    -------
+    mnu : number
+        U-component
+    mnv : number
+        V-component
+
+    '''
+    if pbot == -1: lower = 850.
+    if ptop == -1: upper = 200.
+    pinc = int((pbot - ptop) / psteps)
+    if pinc < 1:
+        u1, v1 = interp.components(pbot, prof)
+        u2, v2 = interp.components(ptop, prof)
+        u1 = (u1 - stu) * pbot
+        v1 = (v1 - stv) * pbot
+        u2 = (u2 - stu) * ptop
+        v2 = (v2 - stv) * ptop
+        usum = u1 + u2
+        vsum = v1 + v2
+        wgt = pbot + ptop
+    else:
+        wgt = 0
+        usum = 0
+        vsum = 0
+        for p in range(int(pbot), int(ptop)+1, -pinc):
+            utmp, vtmp = interp.components(p, prof)
+            usum += (utmp - stu) * p
+            vsum += (vtmp - stv) * p
+            wgt += p
+
+    return usum / wgt, vsum / wgt
+
+
---
+++ sharppy/tests/test_winds.py
@@ -0,0 +1,20 @@
+import numpy as np
+import numpy.ma as ma
+import numpy.testing as npt
+import sharppy.sharptab.winds as winds
+from sharppy.sharptab.profile import Profile
+
+
+p = [1000., 925., 850., 700., 500., 300, 250., 200., 150., 100.]
+z = [161., 798., 1496., 3086., 5720., 9370., 10570., 11970., 13700., 16230.]
+t = [-9999., 1.2, 9.2, 1.6, -15.5, -43.7, -54.5, -64.1, -64.7, -58.7]
+d = [-9999., 1., 9.2, -1.2, -21.5, -50.7, -62.5, -71.1, -71.7, -73.7]
+wd = [-9999., 70., 175., 230., 255., 265., 255., 260., 280., 295.]
+ws = [-9999., 31.99, 27.99, 25.99, 25.99, 48., 52.99, 75., 77., 39.01]
+prof = Profile(pres=p, hght=z, tmpc=t, dwpc=d, wdir=wd, wspd=ws)
+
+
+def test_mean_wind():
+    correct = 20.103278626040353, 14.726131326109194
+    returned = winds.mean_wind(prof)
+    npt.assert_almost_equal(returned, correct)
