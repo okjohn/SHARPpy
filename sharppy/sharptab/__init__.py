ae2990c6584faee3bd74d99c32eee079f5a13bd6
Patrick Marsh <patrickmarshwx@gmail.com>
2013-04-02 21:48:20 -0500

    Added the new interpolation file

--- sharppy/sharptab/__init__.py
+++ sharppy/sharptab/__init__.py
@@ -2,6 +2,7 @@ import constants
 import utils
 import profile
 import thermo
+import interp
 
 
-__all__ = ['contants', 'utils', 'profile', 'thermo']
+__all__ = ['contants', 'utils', 'profile', 'thermo', 'interp']
