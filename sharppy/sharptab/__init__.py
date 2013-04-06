c4f7f433d627785d8281ef0ae7601a1359590ef2
Patrick Marsh <patrickmarshwx@gmail.com>
2013-04-06 12:42:14 -0500

    added wind routines to the import list

--- sharppy/sharptab/__init__.py
+++ sharppy/sharptab/__init__.py
@@ -3,6 +3,6 @@ import utils
 import profile
 import thermo
 import interp
+import winds
 
-
-__all__ = ['contants', 'utils', 'profile', 'thermo', 'interp']
+__all__ = ['contants', 'utils', 'profile', 'thermo', 'interp', 'winds']
